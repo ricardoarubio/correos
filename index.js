d48b9a791218ce6931e6da4aeedb73dbcd599d23
ricardoarubio <ricardoarubio823@gmail.com>
2021-02-03 16:46:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Request
 
 // Node.js SDK: https://github.com/sendinblue/APIv3-nodejs-library
-var SibApiV3Sdk = require('sib-api-v3-sdk');
+/*var SibApiV3Sdk = require('sib-api-v3-sdk');
 var defaultClient = SibApiV3Sdk.ApiClient.instance;
 
 // Configure API key authorization: api-key
@@ -17,10 +17,10 @@ apiInstance.createContact(createContact).then(function(data) {
     console.log('API called successfully. Returned data: ' + data);
 }, function(error) {
     console.error(error);
-});
+});*/
 
 //prueba
-/*var SibApiV3Sdk = require('sib-api-v3-sdk');
+var SibApiV3Sdk = require('sib-api-v3-sdk');
 
 var defaultClient = SibApiV3Sdk.ApiClient.instance;
 
@@ -41,4 +41,4 @@ api.getAccount().then(function(data) {
     console.log('API called successfully. Returned data: ' + data);
 }, function(error) {
     console.error(error);
-});*/
+});
