442be3da4c4993fa8f7265280ee27455a84d9d0f
ricardoarubio <ricardoarubio823@gmail.com>
2021-02-03 11:18:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ apiKey.apiKey = 'xkeysib-eb0a5174e7362a12201d34134321b0c432d9ce0d9652c5f2202c670
 var apiInstance = new SibApiV3Sdk.ContactsApi();
 
 var createContact = new SibApiV3Sdk.CreateContact(); // CreateContact | Values to create a contact
-createContact = { 'email': "john@doe.com" };
+createContact = { 'email': "ing8@tciconsultoria.com" };
 
 apiInstance.createContact(createContact).then(function(data) {
     console.log('API called successfully. Returned data: ' + data);
@@ -19,4 +19,26 @@ apiInstance.createContact(createContact).then(function(data) {
     console.error(error);
 });
 
-//
+//prueba
+/*var SibApiV3Sdk = require('sib-api-v3-sdk');
+
+var defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+// Configure API key authorization: api-key
+var apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = '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';
+// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+//apiKey.apiKeyPrefix['api-key'] = "Token"
+
+// Configure API key authorization: partner-key
+var partnerKey = defaultClient.authentications['partner-key'];
+partnerKey.apiKey = "YOUR API KEY"
+    // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+    //partnerKey.apiKeyPrefix['partner-key'] = "Token"
+
+var api = new SibApiV3Sdk.AccountApi()
+api.getAccount().then(function(data) {
+    console.log('API called successfully. Returned data: ' + data);
+}, function(error) {
+    console.error(error);
+});*/
