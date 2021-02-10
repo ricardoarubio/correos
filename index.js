30b6cafe89a9ee999b9e6165884a8a52ba37ca0d
ricardoarubio <ricardoarubio823@gmail.com>
2021-02-10 10:40:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //Request
 
 // Node.js SDK: https://github.com/sendinblue/APIv3-nodejs-library
-/*var SibApiV3Sdk = require('sib-api-v3-sdk');
-var defaultClient = SibApiV3Sdk.ApiClient.instance;
+var SibApiV3Sdk = require('sib-api-v3-sdk');
+/*var defaultClient = SibApiV3Sdk.ApiClient.instance;
 
 // Configure API key authorization: api-key
 var apiKey = defaultClient.authentications['api-key'];
@@ -20,7 +20,6 @@ apiInstance.createContact(createContact).then(function(data) {
 });*/
 
 //prueba
-var SibApiV3Sdk = require('sib-api-v3-sdk');
 
 var defaultClient = SibApiV3Sdk.ApiClient.instance;
 
@@ -32,13 +31,13 @@ apiKey.apiKey = 'xkeysib-eb0a5174e7362a12201d34134321b0c432d9ce0d9652c5f2202c670
 
 // Configure API key authorization: partner-key
 var partnerKey = defaultClient.authentications['partner-key'];
-partnerKey.apiKey = "YOUR API KEY"
+partnerKey.apiKey = "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"
     // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
     //partnerKey.apiKeyPrefix['partner-key'] = "Token"
 
 var api = new SibApiV3Sdk.AccountApi()
 api.getAccount().then(function(data) {
-    console.log('API called successfully. Returned data: ' + data);
+    console.log('API called successfully. Returned data: ' + data + ' Hola');
 }, function(error) {
     console.error(error);
 });
